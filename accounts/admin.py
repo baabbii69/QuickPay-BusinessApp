eedf21a318a3214445f0fd22e7d3898419ef97d3
baabbii <yohanesalemu0069@gmail.com>
2023-04-13 14:30:07 +0300

    Models Done

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register((Industry, Incorporation, States, BusnessAdress, IdDocument, PesonalAdderss, DedicatedPerson, VerifyBusiness))
--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,32 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 
+
+staff_sizes = (("1-5 People", "1-5 People"), ("5-10 People", "5-10 People"), ("More Than 10 People", "More Than 10 People"))
+Transaction_Volume = (("10,000 - 100,000", "10,000 - 100,000"), ("100,000 - 500,000", "100,000 - 500,000"), ("More Than 500,000", "More Than 500,000"))
+genders = (("Male", "Male"), ("Female", "Female"))
+id_type =(("Passport", "Passport"), ("Driver License", "Driver License"), ("kebele ID", "kebele ID"), ("National ID", "National ID"))
+
 class UserAccountManager(BaseUserManager):
+	def create_superuser(self, email, password, **extra_fields):
+		extra_fields.setdefault('is_staff', True)
+		extra_fields.setdefault('is_superuser', True)
+		extra_fields.setdefault('is_active', True)
+
+		if extra_fields.get("is_staff") is not True:
+			raise ValueError("Superuser must have is_staff=True.")
+
+		if extra_fields.get("is_superuser") is not True:
+			raise ValueError("Superuser must have is_superuser=True.")
+
+		if not email:
+			raise ValueError("Email field is required")
+
+		user = self.model(email=email, **extra_fields)
+		user.set_password(password)
+		user.save()
+		return user
+
 	def create_user(self, email, password=None,  **extra_fields):
 		if not email:
 			raise ValueError('Email Field is required to open an Account')
@@ -15,14 +40,17 @@ class UserAccountManager(BaseUserManager):
 
 		return user
 
+	
+
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
 	email = models.EmailField(max_length=255, unique=True)
 	first_name = models.CharField(max_length=255)
 	last_name = models.CharField(max_length=255)
 	business_name = models.CharField(max_length=255, unique=True)
-	is_active = models.BooleanField(default=True)
 	is_staff = models.BooleanField(default=False)
+	is_superuser = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=True)
 
 	objects = UserAccountManager()
 
@@ -33,7 +61,85 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 		return self.first_name + ' ' + self.last_name
 
 	def get_short_name(self):
-		return self.name
+		return self.first_name
 
 	def __str__(self):
 		return self.email
+
+
+class Industry(models.Model):
+	industry_name = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.industry_name
+
+class Incorporation(models.Model):
+	incorp_name = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.incorp_name
+
+class States(models.Model):
+	state_name = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.state_name
+
+class BusnessAdress(models.Model):
+	states = models.ForeignKey(States, on_delete=models.PROTECT, related_name="states")
+	kifle_ketema = models.CharField(max_length=25)
+	woreda = models.CharField(max_length=20)
+	kebele = models.CharField(max_length=5)
+	house_number = models.CharField(max_length=20)
+	frendly_BN = models.CharField(max_length=50)
+	business_phone = models.CharField(max_length=12)
+	business_email = models.EmailField(max_length=255, unique=True)
+
+
+	def __str__(self):
+		return self.states
+
+class IdDocument(models.Model):
+	ids = models.CharField(max_length=20, choices=id_type)
+	id_upload = models.ImageField(upload_to='ids/')
+
+
+class PesonalAdderss(models.Model):
+	full_address = models.CharField(max_length=100)
+	state = models.ForeignKey(States, on_delete=models.PROTECT, related_name="state")
+
+
+	def __str__(self):
+		return self.full_address
+
+class DedicatedPerson(models.Model):
+	gender = models.CharField(max_length=15, choices=genders)
+	date_of_birth = models.DateField()
+	address = models.ForeignKey(PesonalAdderss, on_delete=models.PROTECT, related_name="personal_adress")
+	id_docouments = models.ForeignKey(IdDocument, on_delete=models.PROTECT, related_name="id_docouments")
+
+
+
+class VerifyBusiness(models.Model):
+	business_type = models.BooleanField(default=True)
+	industrys = models.ForeignKey(Industry, on_delete=models.PROTECT, related_name="industrys")
+	category = models.CharField(max_length=30)
+	staff_size = models.CharField(max_length=30, choices=staff_sizes)
+	trans_volume = models.CharField(max_length=30, choices=Transaction_Volume)
+	legal_BN = models.CharField(max_length=50, null=False)
+	tin_numbers = models.CharField(max_length=50, null=False)
+	vat_check = models.BooleanField(default=False)
+	business_reg_num = models.CharField(max_length=30)
+	incorp_type = models.ForeignKey(Incorporation, on_delete=models.PROTECT, related_name="incorporation_name")
+	trade_license = models.FileField(upload_to='trade_license/')
+	tin_certificate = models.FileField(upload_to='tin_certificate/')
+	memorandum = models.FileField(upload_to='memorandum/')
+	business_address = models.ForeignKey(BusnessAdress, on_delete=models.PROTECT)
+	business_contact = models.CharField(max_length=30)
+	proof_address = models.FileField(upload_to='address_proof/')
+
+
+	def __str__(self):
+		return self.legal_BN
+
+
